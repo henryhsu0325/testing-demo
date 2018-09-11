f19ae79df645eba836ba7cfb6e9e65e4f03c2f53
Henry <henry81325@gmail.com>
2018-09-11 12:47:59 +0800

    multi combo all need discount

--- potter-kata/lib/cart.rb
+++ potter-kata/lib/cart.rb
@@ -10,14 +10,20 @@ class Cart
     total = 0
     @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
 
-    if @ordered_items.size == 2 
-      total = total + 2 * PRICE * 0.95
+    if @ordered_items.size == 2
+      while @ordered_items.size > 0
+        total = total + 2 * PRICE * 0.95
+        @ordered_items[0] -= 1
+        @ordered_items[1] -= 1
+        @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+      end
     else
       for item in @ordered_items do
         total = total + item * PRICE
       end
     end
-    
+
     return total
   end
 end
+
