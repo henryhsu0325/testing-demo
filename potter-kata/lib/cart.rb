032754b9a976ec55a63bb30306d0800b7a26eeb1
Henry <henry81325@gmail.com>
2018-09-11 12:56:25 +0800

    multi combo, mix discount and original price

--- potter-kata/lib/cart.rb
+++ potter-kata/lib/cart.rb
@@ -10,20 +10,21 @@ class Cart
     total = 0
     @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
 
-    if @ordered_items.size == 2
-      while @ordered_items.size > 0
+    while @ordered_items.size > 0
+      if @ordered_items.size == 2
         total = total + 2 * PRICE * 0.95
         @ordered_items[0] -= 1
         @ordered_items[1] -= 1
         @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
-      end
-    else
-      for item in @ordered_items do
-        total = total + item * PRICE
+      else
+        total = total + PRICE
+        @ordered_items[0] -= 1
+        @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
       end
     end
 
     return total
   end
+  
 end
 
--- potter-kata/spec/cart_spec.rb
+++ potter-kata/spec/cart_spec.rb
@@ -22,27 +22,25 @@ describe Cart do
 
   context "第二種情境：打 5% 折扣" do
 
-  it "第一集買 1 本、第二集買 1 本" do 
-      @cart.add({ "1st": 1, "2nd": 1, "3rd": 0, "4th": 0, "5th": 0 })
-      expect(@cart.calculate).to eq(190)
-  end
+    it "第一集買 1 本、第二集買 1 本" do 
+        @cart.add({ "1st": 1, "2nd": 1, "3rd": 0, "4th": 0, "5th": 0 })
+        expect(@cart.calculate).to eq(190)
+    end
 
-  it "第一集買 2 本、第二集買 2 本" do 
-      @cart.add({ "1st": 2, "2nd": 2, "3rd": 0, "4th": 0, "5th": 0 })
-      expect(@cart.calculate).to eq(380)
-  end
+    it "第一集買 2 本、第二集買 2 本" do 
+        @cart.add({ "1st": 2, "2nd": 2, "3rd": 0, "4th": 0, "5th": 0 })
+        expect(@cart.calculate).to eq(380)
+    end
 
-  it "第一集買 1 本、第二集買 2 本" do 
-      @cart.add({ "1st": 1, "2nd": 2, "3rd": 0, "4th": 0, "5th": 0 })
-      expect(@cart.calculate).to eq(290)
-  end
+    it "第一集買 1 本、第二集買 2 本" do 
+        @cart.add({ "1st": 1, "2nd": 2, "3rd": 0, "4th": 0, "5th": 0 })
+        expect(@cart.calculate).to eq(290)
+    end
 
-  it "第一集買 2 本、第二集買 3 本" do 
-      @cart.add({ "1st": 2, "2nd": 3, "3rd": 0, "4th": 0, "5th": 0 })
-      expect(@cart.calculate).to eq(480)
-  end
-
-
+    it "第一集買 2 本、第二集買 3 本" do 
+        @cart.add({ "1st": 2, "2nd": 3, "3rd": 0, "4th": 0, "5th": 0 })
+        expect(@cart.calculate).to eq(480)
+    end
 
   end
 
