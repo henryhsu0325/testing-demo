e4ced4ee0a535f59217657899d853ef88119e80f
Henry <henry81325@gmail.com>
2018-09-11 13:23:14 +0800

    refactor

--- potter-kata/lib/cart.rb
+++ potter-kata/lib/cart.rb
@@ -3,28 +3,23 @@ class Cart
   PRICE = 100
 
   def add(items)
-    @ordered_items = items.map{ |key,value| value }
+    @ordered_items = items.map{ |key,value| value }.delete_if{ |i| i == 0 } #將資料轉換為陣列，刪除沒有買的集數
   end
 
   def calculate
     total = 0
-    @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
 
-    while @ordered_items.size > 0
+    while @ordered_items.size > 0 # 當客戶還有書籍尚未結帳時
       if @ordered_items.size == 2
-        total = total + 2 * PRICE * 0.95
-        @ordered_items[0] -= 1
-        @ordered_items[1] -= 1
-        @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+        total = total + 2 * PRICE * 0.95 # 客戶買了一組
       else
-        total = total + PRICE
-        @ordered_items[0] -= 1
-        @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+        total = total + PRICE # 客戶單買了一本
       end
+      @ordered_items = @ordered_items.map{ |i| i -= 1 } # 刪除已計算過的書籍
+      @ordered_items = @ordered_items.delete_if{ |i| i == 0 } # 把已經結完的集數刪除
     end
 
     return total
   end
-  
-end
 
+end
